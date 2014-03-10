2d87576df586e6e6aaf3374c9719ad3e5ad15602
Matt <brownprobable@gmail.com>
2014-03-10 20:25:07 +1300

    Finished views and added routes to app.rb

--- app.rb
+++ app.rb
@@ -15,12 +15,67 @@ helpers do
     end
   end
 
+  def post_show_page?
+    request.path_info =~ /\/posts\/\dd+$/
+  end
+
   def pretty_date(time)
     time.strftime("%d %b %Y")
   end
+
+  def delete_post_button(post_id)
+    erb :_delete_post_button, locals: { post_id: post_id}
+  end
+
 end
 
 get "/" do
   @posts = Post.order("created_at DESC")
   erb :"posts/index"
+end
+
+get "/posts/new" do
+  @title = "New Post"
+  @post = Post.new
+  erb :"posts/new"
+end
+
+post "/posts" do
+  @post = Post.new(params[:posts])
+  if @post.save
+    redirect "posts/#{@post.id}"
+  else
+    erb :"posts/new"
+  end
+end
+
+get "/posts/:id" do
+  @post = Post.find(params[:id])
+  @title = @post.title
+  erb :"posts/show"
+end
+
+get "/posts/:id/edit" do
+  @post = Post.find(params[:id])
+  @title = "Edit Form"
+  erb :"posts/edit"
+end
+
+put "/posts/:id" do
+  @post = Post.find(params[:id])
+  if @post.update_attributes(params[:post])
+    redirect "/posts/#{@post.id}"
+  else
+    erb :"posts/edit"
+  end
+end
+
+delete "posts/:id" do
+  @post = Post.find(params[:id]).destroy
+  redirect "/"
+end
+
+get "/about" do
+  @title = "About Me"
+  erb :"pages.about"
 end
